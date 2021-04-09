0f2a6b968114f62eb9e7984cb17a73d552986df5
An2 <an2m001@gmail.com>
2021-04-09 12:17:30 -0400

    .

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// const exec = () => {
+//   for (let i = 0; i < 100; ) console.log(String.fromCharCode(i));
+// };
+// exec()
+let table = ["ar", "bb", "lite", "cls", "22", 22, 23, 01];
+
+console.log("a");
+console.log("a");
